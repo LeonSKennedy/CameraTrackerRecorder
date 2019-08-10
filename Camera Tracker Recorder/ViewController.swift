73f79a8d65c144f67d9264f001827788eb158319
Michael Levesque <michaelltd@mac.com>
2019-08-10 13:07:53 -0700

    https://github.com/mlevesque/CameraTrackerRecorder/issues/4 Added user interface to change file names for recordings as projectname-scene-take. Take automatically increments when a recording completes to prepare for the next one.

--- Camera Tracker Recorder/ViewController.swift
+++ Camera Tracker Recorder/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 import SceneKit
 import ARKit
+import Foundation
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     @IBOutlet weak var recButton: UIButton!
     
-    var sceneRecorder: SceneRecorder?
-    var useAudio: Bool = false
+    @IBOutlet weak var projectText: UILabel!
+    @IBOutlet weak var sceneText: UILabel!
+    @IBOutlet weak var takeText: UILabel!
+    
+    static let notificationName = Notification.Name("MainViewController")
+    
+    private var sceneRecorder: SceneRecorder?
+    private var useAudio: Bool = false
+    
+    private var projectName: String = "Project Name"
+    private var sceneValue: String = "123A"
+    private var takeValue: Int = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // setup notifications
+        NotificationCenter.default.addObserver(self,
+            selector: #selector(onUpdateTextNotification(notification:)),
+            name: ViewController.notificationName,
+            object: nil)
         
         // Set the view's delegate
         sceneView.delegate = self
@@ -35,6 +52,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
         // Set the scene to the view
         sceneView.scene = scene
+        
+        // Update Text UI
+        updateText()
         
         let audioSession = AVAudioSession.sharedInstance()
         do {
@@ -67,6 +87,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+        
+        // stop recording
+        sceneRecorder?.stopRecording()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // transfer text data to Edit Text view
+        if segue.destination is EditTextViewController {
+            if let vc = segue.destination as? EditTextViewController {
+                vc.setText(project: projectName, scene: sceneValue, take: "\(takeValue)")
+            }
+        }
     }
     
     // MARK: - ARSCNViewDelegate
@@ -95,48 +127,103 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
     }
     
-    
     @IBAction func onRecordTouchUp(_ sender: Any) {
-        let isRecording: Bool = sceneRecorder?.isRecording ?? false
+        let isRecording = sceneRecorder?.isRecording ?? false
         
         do {
             if isRecording {
+                // stop recording and save data
                 sceneRecorder?.stopRecording()
-                prepareRecorder()
+                
+                // prepare for next take
+                incrementTake()
             }
             else {
+                // @TODO Add warning if overwriting file
                 try sceneRecorder?.startRecording()
             }
         }
         catch {
-            print(error)
+            // @TODO display error
         }
         
+        // update UI
         updateRecordButton()
     }
     
+    private func incrementTake() {
+        // increment value
+        takeValue = takeValue + 1
+        
+        // setup new recorder
+        prepareRecorder()
+        
+        // update UI
+        updateText()
+    }
+    
     private func prepareRecorder() {
+        // stop and previous recording
+        sceneRecorder?.stopRecording()
+        
+        // create new recorder with new name
         do {
-            var sceneDataRecorder: SceneRecorder = try SceneDataRecorder(name: "test", arSession: sceneView.session)
+            var sceneDataRecorder: SceneRecorder = try SceneDataRecorder(name: getRecorderName(), arSession: sceneView.session)
             if useAudio {
                 sceneDataRecorder = try SceneAudioRecorderDecorator(sceneRecorder: sceneDataRecorder)
             }
             sceneRecorder = sceneDataRecorder
         }
         catch {
-            
+            // @TODO display error
         }
+        
+        // @TODO handle if file already exists with new name
+    }
+    
+    private func getRecorderName() -> String {
+        return "\(projectName)-\(sceneValue)-\(takeValue)"
     }
     
     private func updateRecordButton() {
+        // update look of record button based on whether or not we are recording
         let isRecording = sceneRecorder?.isRecording ?? false
+        
+        // display button as stop button if currently recording
         if isRecording {
             recButton.backgroundColor = UIColor.gray
             recButton.setTitle("Stop", for: UIControl.State.normal)
         }
+            
+        // display as red record button if not currently recording
         else {
             recButton.backgroundColor = UIColor.red
             recButton.setTitle("Record", for: UIControl.State.normal)
         }
+    }
+    
+    private func updateText() {
+        projectText.text = projectName
+        sceneText.text = sceneValue
+        takeText.text = "\(takeValue)"
+    }
+    
+    @objc private func onUpdateTextNotification(notification: Notification) {
+        // set new values
+        if let project = notification.userInfo?[TextKeys.ProjectName] as? String {
+            projectName = project
+        }
+        if let scene = notification.userInfo?[TextKeys.SceneValue] as? String {
+            sceneValue = scene
+        }
+        if let take = notification.userInfo?[TextKeys.TakeValue] as? String {
+            takeValue = Int(take) ?? takeValue
+        }
+        
+        // setup recorder with the new name
+        prepareRecorder()
+        
+        // update the UI
+        updateText()
     }
 }
